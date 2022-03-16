d692f764bab10e4b360de40287ed288ace0d8c31
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-03-16 20:43:34 -0300

    Configuration file change
    modification in ros_control configuration files
     and add comments to the "robot_base.ino" file

--- hera_base/arduino/robot_base/robot_base.ino
+++ hera_base/arduino/robot_base/robot_base.ino
@@ -12,35 +12,39 @@
 
 #define ODOM_PERIOD 50 // 20 Hz
 
-Sabertooth STFront(128);
-Sabertooth STRear(129);
+// importa o driver do sabertooth
+Sabertooth STFront(128); 
+Sabertooth STRear(129); /
 
 float V1, V2, V3, V4;
 
-Encoder Encoder1(22, 23);
+// importa o driver do encoder
+Encoder Encoder1(22, 23);  
 Encoder Encoder2(32, 33);
 Encoder Encoder3(42, 43);
 Encoder Encoder4(50, 51);
 
-long odom_timer;
+long odom_timer; // timer para o odometro
 
+// Calcule a diferença entre o valor que queremos mapear para o intervalo de entrada e o valor mínimo do intervalo de entrada 
 float map(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+// Converte o valor de entrada de joystick para motor
 int convertToMotor(float value) {
   float r = map(value, -1.085, 1.117, -127.0, 127.0);
   r = constrain(r, -127.0, 127.0);
   return (int) round(r);
 }
-
+// Ajusta a velocidade dos motores para os valores de Power1, Power2, Power3 e Power4
 void moveMotors(int Power1, int Power2, int Power3, int Power4) {
   STFront.motor(1, Power1);
   STFront.motor(2, Power2);
   STRear.motor(1, Power3);
   STRear.motor(2, Power4);
 }
-
+// Ajusta os motores para potência zero
 void stopMotors() {
   STFront.motor(1, 0);
   STFront.motor(2, 0);
@@ -48,6 +52,7 @@ void stopMotors() {
   STRear.motor(2, 0);
 }
 
+// Recebe o comando de velocidade do tópico cmd_vel e converte para velocidade do motor
 void cmd_vel_callback(const geometry_msgs::Twist& vel)
 {
   // Mecanum Kinematics
@@ -59,8 +64,10 @@ void cmd_vel_callback(const geometry_msgs::Twist& vel)
   moveMotors(convertToMotor(V1), convertToMotor(V2), convertToMotor(V3), convertToMotor(V4));
 }
 
+// Crie um assinante que escute o tópico "cmd_vel"
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", cmd_vel_callback);
 
+// Define os codificadores para 0
 void clearEncoders() {
   Encoder1.write(0);
   Encoder2.write(0);
@@ -90,7 +97,7 @@ void setup() {
 
   odom_timer = millis();
 }
-
+// Leia os valores do codificador e coloque-os no encoder_msg
 void loop() {
   if (millis() - odom_timer >= ODOM_PERIOD) {
     enc_msg.data[0] = Encoder1.read();
@@ -104,6 +111,6 @@ void loop() {
 
     clearEncoders();
   }
-
+  // Cria um nó com o nome "talke"
   nh.spinOnce();
 }
